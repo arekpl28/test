80c630056310ffc41b4cc8d74912f74738106f2d
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-06-14 18:29:52 +0200

    update

--- src/layouts/home_page/Section5.js
+++ src/layouts/home_page/Section5.js
@@ -7,39 +7,6 @@ import Art5 from "./articles/Art5.js";
 import Art6 from "./articles/Art6.js";
 import Square from "./articles/Square";
 
-// const arrArt = [
-//   {
-//     id: 0,
-//     name: Art1,
-//     active: true
-//   },
-//   {
-//     id: 1,
-//     name: Art2,
-//     active: false
-//   },
-//   {
-//     id: 2,
-//     name: Art3,
-//     active: false
-//   },
-//   {
-//     id: 3,
-//     name: Art4,
-//     active: false
-//   },
-//   {
-//     id: 4,
-//     name: Art5,
-//     active: false
-//   },
-//   {
-//     id: 5,
-//     name: Art6,
-//     active: false
-//   }
-// ];
-
 class Section5 extends Component {
   state = {
     isDown: false,
@@ -91,7 +58,64 @@ class Section5 extends Component {
     });
   };
 
+  handleMouseMove = e => {
+    const inner = window.innerWidth;
+    const innerX = ((inner / 2) * 100) / (inner + 110);
+    e.preventDefault();
+    if (this.state.isDown) {
+      let touch = e.pageX;
+      let scrollLeft = this.state.startX - touch;
+      // let touch = e.touches[0];
+      // let scrollLeft = this.state.startX - touch.pageX;
+      if (scrollLeft < 0) {
+        return;
+      }
+      if (scrollLeft < 10 && scrollLeft > 0) {
+        this.setState({
+          scrollLeft: 0,
+          number: 0
+        });
+      }
+      if (scrollLeft > 10 && scrollLeft < innerX) {
+        this.setState({
+          scrollLeft: 100,
+          number: 1
+        });
+      }
+      if (scrollLeft > 110 && scrollLeft < innerX * 2) {
+        this.setState({
+          scrollLeft: 200,
+          number: 2
+        });
+      }
+      if (scrollLeft > 210 && scrollLeft < innerX * 3) {
+        this.setState({
+          scrollLeft: 300,
+          number: 3
+        });
+      }
+      if (scrollLeft > 310 && scrollLeft < innerX * 4) {
+        this.setState({
+          scrollLeft: 400,
+          number: 4
+        });
+      }
+      if (scrollLeft > innerX * 5) {
+        this.setState({
+          scrollLeft: 500,
+          number: 5
+        });
+      }
+      this.setState({
+        scrollLeft: "-" + scrollLeft.toFixed()
+      });
+      this.changeSquare();
+    }
+  };
+
   handleMouseUp = () => {
+    const inner = window.innerWidth;
+    const innerX = ((inner / 2) * 100) / (inner + 110);
     if (this.state.scrollLeft > -10) {
       this.setState({
         scrollLeft: 0
@@ -99,27 +123,32 @@ class Section5 extends Component {
     }
     if (this.state.scrollLeft < -10) {
       this.setState({
-        scrollLeft: -100
+        scrollLeft: -innerX,
+        number: 1
       });
     }
-    if (this.state.scrollLeft < -110) {
+    if (this.state.scrollLeft < -(innerX * 2 + 10)) {
       this.setState({
-        scrollLeft: -200
+        scrollLeft: -(innerX * 2),
+        number: 2
       });
     }
-    if (this.state.scrollLeft < -210) {
+    if (this.state.scrollLeft < -(innerX * 3 + 10)) {
       this.setState({
-        scrollLeft: -300
+        scrollLeft: -(innerX * 3),
+        number: 3
       });
     }
-    if (this.state.scrollLeft < -310) {
+    if (this.state.scrollLeft < -(innerX * 4 + 10)) {
       this.setState({
-        scrollLeft: -400
+        scrollLeft: -(innerX * 4),
+        number: 4
       });
     }
-    if (this.state.scrollLeft < -410) {
+    if (this.state.scrollLeft < -(innerX * 5 + 10)) {
       this.setState({
-        scrollLeft: -500
+        scrollLeft: -(innerX * 5),
+        number: 5
       });
     }
     this.setState({
@@ -127,19 +156,6 @@ class Section5 extends Component {
     });
   };
 
-  handleMouseMove = e => {
-    e.preventDefault();
-    if (this.state.isDown) {
-      let touch = e.pageX;
-      let scrollLeft = this.state.startX - touch;
-      if (scrollLeft < 0) {
-        return;
-      }
-      this.setState({
-        scrollLeft: "-" + scrollLeft.toFixed()
-      });
-    }
-  };
   handleTouchStart = e => {
     clearInterval(this.indexInterval);
     const startX = e.touches[0].pageX;
@@ -147,6 +163,7 @@ class Section5 extends Component {
       startX: startX - this.state.scrollLeft
     });
   };
+
   handleTouchMove = e => {
     let touch = e.touches[0];
     let scrollLeft = this.state.startX - touch.pageX;
@@ -194,6 +211,7 @@ class Section5 extends Component {
       scrollLeft: "-" + scrollLeft.toFixed()
     });
   };
+
   handleTouchEnd = () => {
     if (this.state.scrollLeft > -10) {
       this.setState({
@@ -232,6 +250,7 @@ class Section5 extends Component {
       });
     }
   };
+
   changeSquare = () => {
     const activeSquare = this.state.arrArt.findIndex(
       square => square.active === true
@@ -257,15 +276,29 @@ class Section5 extends Component {
       arrArt: x
     });
   };
+
   componentDidMount() {
+    const inner = window.innerWidth;
+    const innerX = ((inner / 2) * 100) / (inner + 110);
     this.indexInterval = setInterval(() => {
-      this.setState(prevState => ({
-        scrollLeft: prevState.scrollLeft - 100
-      }));
-      if (this.state.scrollLeft === -600) {
-        this.setState({
-          scrollLeft: 0
-        });
+      if (inner < 1023) {
+        this.setState(prevState => ({
+          scrollLeft: prevState.scrollLeft - 100
+        }));
+        if (this.state.scrollLeft === -600) {
+          this.setState({
+            scrollLeft: 0
+          });
+        }
+      } else {
+        this.setState(prevState => ({
+          scrollLeft: prevState.scrollLeft - innerX
+        }));
+        if (this.state.number === 0) {
+          this.setState({
+            scrollLeft: 0
+          });
+        }
       }
       this.changeSquare();
       this.setState(prevState => ({
@@ -278,46 +311,88 @@ class Section5 extends Component {
       }
     }, 2000);
   }
+
   handleClick = id => {
-    if (id === 0) {
-      this.setState({
-        scrollLeft: 0,
-        number: id
-      });
+    const inner = window.innerWidth;
+    const innerX = ((inner / 2) * 100) / (inner + 110);
+    if (inner < 1025) {
+      if (id === 0) {
+        this.setState({
+          scrollLeft: 0,
+          number: id
+        });
+      }
+      if (id === 1) {
+        this.setState({
+          scrollLeft: -100,
+          number: id
+        });
+      }
+      if (id === 2) {
+        this.setState({
+          scrollLeft: -200,
+          number: id
+        });
+      }
+      if (id === 3) {
+        this.setState({
+          scrollLeft: -300,
+          number: id
+        });
+      }
+      if (id === 4) {
+        this.setState({
+          scrollLeft: -400,
+          number: id
+        });
+      }
+      if (id === 5) {
+        this.setState({
+          scrollLeft: -500,
+          number: id
+        });
+      }
+    } else {
+      if (id === 0) {
+        this.setState({
+          scrollLeft: 0,
+          number: id
+        });
+      }
+      if (id === 1) {
+        this.setState({
+          scrollLeft: -innerX,
+          number: id
+        });
+      }
+      if (id === 2) {
+        this.setState({
+          scrollLeft: -(innerX * 2),
+          number: id
+        });
+      }
+      if (id === 3) {
+        this.setState({
+          scrollLeft: -(innerX * 3),
+          number: id
+        });
+      }
+      if (id === 4) {
+        this.setState({
+          scrollLeft: -(innerX * 4),
+          number: id
+        });
+      }
+      if (id === 5) {
+        this.setState({
+          scrollLeft: -(innerX * 5),
+          number: id
+        });
+      }
     }
-    if (id === 1) {
-      this.setState({
-        scrollLeft: -100,
-        number: id
-      });
-    }
-    if (id === 2) {
-      this.setState({
-        scrollLeft: -200,
-        number: id
-      });
-    }
-    if (id === 3) {
-      this.setState({
-        scrollLeft: -300,
-        number: id
-      });
-    }
-    if (id === 4) {
-      this.setState({
-        scrollLeft: -400,
-        number: id
-      });
-    }
-    if (id === 5) {
-      this.setState({
-        scrollLeft: -500,
-        number: id
-      });
-    }
-    this.changeSquare1(id);
+    this.changeSquareClick(id);
   };
-  changeSquare1 = id => {
+  changeSquareClick = id => {
     clearInterval(this.indexInterval);
     const activeSquare = this.state.arrArt.findIndex(
       square => square.active === true
--- src/layouts/home_page/articles/Art1.js
+++ src/layouts/home_page/articles/Art1.js
@@ -17,63 +17,15 @@ const Art1 = props => {
           gjennom både Norkart sin løsning og Ambita Infoland, og er svært
           fornøyd med løsninga. Vi sparer masse tid og det har effektivisert vår
           arbeidsdag betraktelig, da de fleste rapportene hentes automatisk.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nihil
-          molestias, incidunt inventore nam nisi fugit itaque! Qui velit sint
-          nesciunt exercitationem molestias sapiente iure enim illum consectetur
-          recusandae, dolores laborum non, natus, blanditiis nisi sed
-          accusantium inventore odit! Natus reprehenderit quaerat eaque soluta
-          amet doloribus laboriosam minima, officiis neque vel esse ullam
-          tempora aspernatur ea id asperiores. Cum rerum maiores fuga nisi
-          quaerat numquam quasi enim, aliquam quam veritatis voluptatibus
-          voluptates quidem inventore quos minima quisquam omnis in itaque ullam
-          soluta natus sequi ex! Nostrum earum laborum est ab dolor eaque
-          fugiat, ipsam corporis quo consequuntur nemo magni illum inventore
-          illo ipsa eius odit. Rem neque, officiis possimus asperiores iste
-          consectetur ipsa? Soluta quis placeat id, facilis et dolor vitae odit
-          dolores? Velit, veniam dolor. Temporibus obcaecati exercitationem
-          voluptates reprehenderit perspiciatis laboriosam atque architecto
-          illum quia laudantium commodi magni sequi labore quam incidunt et
-          accusamus, sunt perferendis amet itaque quo excepturi? Sint aliquam
-          voluptatem iure explicabo voluptatibus facere fugit tenetur ad enim.
-          Repudiandae iste veritatis rerum beatae odio hic officiis laudantium
-          iusto! Numquam, sit, cum eaque iste deserunt, unde minima in ad
-          possimus expedita eligendi! Ut dolore, accusantium excepturi saepe
-          quaerat eveniet sed! Cupiditate sit, laudantium placeat non autem
-          optio quo, beatae molestias dolor culpa quam quidem perferendis?
-          Eligendi rerum earum quas non dolorem ducimus blanditiis inventore
-          consectetur velit ad fugiat doloribus illo reprehenderit suscipit quos
-          aliquid vel, ratione dignissimos deserunt doloremque dolore animi sit.
-          Repellendus reiciendis hic eius recusandae nobis autem minima nulla,
-          in ad sit, obcaecati doloribus tempora fugit fuga, error ut aliquam
-          veniam quia at facere accusamus delectus qui? Reprehenderit at eaque,
-          laudantium expedita, molestias vel sapiente harum qui neque,
-          consectetur doloribus nostrum repellendus pariatur veniam deserunt
-          corporis iusto sunt? Accusantium iusto id et quos. Ad esse autem ea
-          aliquid, eligendi doloribus beatae, aut minima id sint consectetur
-          repudiandae aperiam, ducimus earum! Consectetur rem ullam, eos magnam
-          facere iure earum omnis doloremque expedita accusantium alias odio est
-          explicabo qui eligendi molestiae libero assumenda quisquam a
-          voluptatem, hic laborum blanditiis minus excepturi! Vitae, voluptate
-          similique? Fugit impedit, vel, eius exercitationem itaque tempore
-          deserunt voluptatibus beatae illum suscipit repellat et libero
-          delectus non magni saepe consequuntur provident? Laboriosam
-          praesentium architecto dolor harum rerum quam ab ea perspiciatis
-          voluptatibus? Necessitatibus, eius itaque. Consectetur esse,
-          necessitatibus, modi hic unde nisi fugit molestias ipsa nihil
-          perferendis minus ad! Facilis, et officia saepe accusantium, ea unde
-          sint, est corporis sapiente mollitia doloribus. Aliquam consequatur
-          modi perspiciatis! Odit sint incidunt assumenda praesentium esse
-          adipisci molestiae dolor nam et voluptas! Delectus itaque natus
-          doloribus asperiores laboriosam beatae facere quos cum, dolorem
-          laborum. Maxime, unde, sapiente voluptatem perspiciatis fugit
-          consectetur neque fugiat asperiores aliquam pariatur temporibus
-          obcaecati libero delectus doloremque possimus alias odio sint in
-          quaerat. Rem asperiores architecto dolore nihil aliquid et illo ipsa
-          accusamus natus commodi, temporibus reiciendis optio saepe qui
-          doloremque recusandae soluta ipsam, nostrum aspernatur nemo? Labore,
-          consectetur optio. Voluptate, amet dignissimos incidunt perspiciatis
-          porro unde delectus, repellendus iure corporis, blanditiis debitis a
-          vitae rerum quibusdam exercitationem tenetur maxime! Unde, molestiae.
+          <br />
+          <br />
+          Ettersom vi også har digitalisert vårt byggesaksarkiv trenger vi ikke
+          lenger å forlate kontorplassen og «springe huset rundt» for å samle de
+          nødvendige opplysningene. Det hele er gjort på ca. 10 minutter. Vi
+          opplever også at meglerne er svært fornøyde med løsninga da de får
+          raskt svar, og vi er ikke lenger avhengig av enkeltpersoner.
+          Opplysningene hentes direkte fra våre fagsystemer, og det gjør at
+          feilkildene er minimale.
         </div>
         <div className="author-infobox">
           <p className="author-name">
